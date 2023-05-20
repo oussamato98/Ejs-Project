f50938d933250d2b72905055d73f341822992acf
oussamato98 <oussamaaouad98@gmail.com>
2023-05-20 17:09:42 +0100

    18 c solved

--- app.js
+++ app.js
@@ -3,6 +3,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { functions } = require("lodash");
+const posts = [
+  Obj = {
+    text : "Test1" ,
+    post : "lkdkdkdkldkldkldlkdlk"
+  }
+];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,8 +23,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-
-  res.render("home",{ homeContent : homeStartingContent});
+  //console.log(posts);
+  res.render("home",{ homeContent : homeStartingContent , postsContent : posts});
 })
 
 app.get("/about",function(req,res){
@@ -31,6 +38,42 @@ app.get("/contact",function(req,res){
   res.render("contact",{ homeContent : contactContent});
   
 })
+
+app.get("/compose",function(req,res){
+
+  res.render("compose");
+  
+})
+
+app.post("/compose",function(req,res){
+  let postObj = {
+    text : req.body.textinput ,
+    post : req.body.postinput
+  }
+  posts.push(postObj);
+
+
+  res.redirect("/");
+
+})
+
+app.get("/posts/:value",function(req,res){
+
+/*     posts.forEach(function(element){
+    console.log(element); }); */
+  let value = req.params.value ;
+  posts.forEach(function(element){
+    if(element.text===value){
+      console.log("match");
+    }
+    else{
+      console.log("dont match")
+    }
+  });
+  
+
+  
+}) ;
 
 
 
@@ -46,3 +89,5 @@ app.get("/contact",function(req,res){
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
